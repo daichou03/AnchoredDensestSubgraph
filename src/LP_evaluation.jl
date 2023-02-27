887a9febc289334f312b0d9f25d44ff3bba2871f
daichou03 <daichou03@gmail.com>
2023-02-27 18:29:17 +1300

    Evaluate comparison results

--- src/LP_evaluation.jl
+++ src/LP_evaluation.jl
@@ -6,14 +6,16 @@ include("LP_consts.jl")
 
 EVAL_ALPHA_EQUAL = 1
 EVAL_ALPHA_DIFF = 2
-EVAL_EXT_TIME_DIFF = 3
-EVAL_INT_TIME_DIFF = 4
-EVAL_LN1 = 5
-EVAL_LM1 = 6
-EVAL_LN2 = 7
-EVAL_LM2 = 8
+EVAL_EXT_TIME_1 = 3
+EVAL_EXT_TIME_2 = 4
+EVAL_INT_TIME_1 = 5
+EVAL_INT_TIME_2 = 6
+EVAL_LN1 = 7
+EVAL_LM1 = 8
+EVAL_LN2 = 9
+EVAL_LM2 = 10
 EVAL_LAST = EVAL_LM2
-EVAL_NAMES = ["alpha_equal", "alpha_diff", "ext_time", "int_time", "ln1", "lm1", "ln2", "lm2"]
+EVAL_NAMES = ["alpha_equal", "alpha_diff", "ext_time_1", "ext_time_2", "int_time_1", "int_time_2", "ln1", "lm1", "ln2", "lm2"]
 FOLDER_LP_EVAL_RESULTS = "../LPEvalResults/"
 
 # suffixName: either a string, or an array containing a suffix for each solverID.
@@ -26,8 +28,10 @@ function CompareResultSets(dataName::String, suffixName)
     nrows = min(nrow(dfs[1]), nrow(dfs[2]))
     alphaEquals = Array{Float64}(undef, nrows)
     alphaDiffs = Array{Float64}(undef, nrows)
-    timeExtDiffs = Array{Float64}(undef, nrows)
-    timeIntDiffs = Array{Float64}(undef, nrows)
+    timeExt1 = Array{Float64}(undef, nrows)
+    timeExt2 = Array{Float64}(undef, nrows)
+    timeInt1 = Array{Float64}(undef, nrows)
+    timeInt2 = Array{Float64}(undef, nrows)
     lns1 = Array{Float64}(undef, nrows)
     lms1 = Array{Float64}(undef, nrows)
     lns2 = Array{Float64}(undef, nrows)
@@ -35,14 +39,16 @@ function CompareResultSets(dataName::String, suffixName)
     for i in 1:nrows
         alphaEquals[i] = almostEqual(dfs[1][i,STATS_NAMES[STATS_ALPHA]], dfs[2][i,STATS_NAMES[STATS_ALPHA]]) ? 1 : 0
         alphaDiffs[i] = dfs[2][i,STATS_NAMES[STATS_ALPHA]] / dfs[1][i,STATS_NAMES[STATS_ALPHA]]
-        timeExtDiffs[i] = dfs[2][i,STATS_NAMES[STATS_EXT_TIME]] / dfs[1][i,STATS_NAMES[STATS_EXT_TIME]]
-        timeIntDiffs[i] = dfs[2][i,STATS_NAMES[STATS_INT_TIME]] / dfs[1][i,STATS_NAMES[STATS_INT_TIME]]
+        timeExt1[i] = dfs[1][i,STATS_NAMES[STATS_EXT_TIME]]
+        timeExt2[i] = dfs[2][i,STATS_NAMES[STATS_EXT_TIME]]
+        timeInt1[i] = dfs[1][i,STATS_NAMES[STATS_INT_TIME]]
+        timeInt2[i] = dfs[2][i,STATS_NAMES[STATS_INT_TIME]]
         lns1[i] = dfs[1][i,STATS_NAMES[STATS_LNSIZE]]
         lms1[i] = dfs[1][i,STATS_NAMES[STATS_LMSIZE]]
         lns2[i] = dfs[2][i,STATS_NAMES[STATS_LNSIZE]]
         lms2[i] = dfs[2][i,STATS_NAMES[STATS_LMSIZE]]
     end
-    return alphaEquals, alphaDiffs, timeExtDiffs, timeIntDiffs, lns1, lms1, lns2, lms2
+    return alphaEquals, alphaDiffs, timeExt1, timeExt2, timeInt1, timeInt2, lns1, lms1, lns2, lms2
 end
 
 function OutputCompareResults(evalResults, dataName::String, suffixName::String)
@@ -63,6 +69,27 @@ function CompareAndOutputResultSets(dataName::String, suffixName)
     evalResults = CompareResultSets(dataName, suffixName)
     OutputCompareResults(evalResults, dataName, suffixName isa String ? suffixName : suffixName[1])
 end
+
+function BulkCompareAndOutputResultSets(dataNames, suffixName)
+    for dataName in dataNames
+        CompareAndOutputResultSets(dataName, suffixName)
+    end
+end
+
+function BulkCompareAndOutputConcatenatedResultSets(dataNames, concatName, suffixName)
+    resultCon = Array{Any}(undef, EVAL_LAST)
+    for i in 1:EVAL_LAST
+        resultCon[i] = []
+    end
+    for dataName in dataNames
+        result = CompareResultSets(dataName, suffixName)
+        for i in 1:EVAL_LAST
+            append!(resultCon[i], result[i])
+        end
+    end
+    OutputCompareResults(resultCon, concatName, suffixName)
+end
+
 
 # TODO:
 # Also output LM, LN (have minimal difference, take both first), regression on these.

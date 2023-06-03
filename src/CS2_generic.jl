283bc145d201ad85e434048553961fb9a7cad6ab
daichou03 <daichou03@gmail.com>
2023-06-03 22:39:06 +1200

    Some extension

--- src/CS2_generic.jl
+++ src/CS2_generic.jl
@@ -13,6 +13,7 @@ FOLDER_CS_LP = "../CaseStudy/LP/"
 
 dataName = "flixster"
 suffixNames = ["FN100","ADSLsmartL","ADSFsmartL","ADSIsmartL","ADSLSsmartL","ADSFSsmartL","ADSISsmartL"]
+suffixNames = ["FN100","ADSLsmartL","ADSFsmartL","ADSLSsmartL","ADSFSsmartL"]
 queryID = 11
 B = readIN(string(dataName, ".in"))
 
@@ -22,7 +23,10 @@ B = readIN(string(dataName, ".in"))
 # Each line represents a node, contains:
 # id,orgid,degree,(whether in...)R,ADS,LPA,...,LIAS
 
-function generateGephi(B::SparseMatrixCSC, dataName::String, suffixNames::Array{String}, queryID::Int64)
+indicatorIDs = [3,4,5]
+# indicatorIDs = [3,4,0]
+
+function generateGephi(B::SparseMatrixCSC, dataName::String, suffixNames::Array{String}, queryID::Int64, suffix::String)
     results = Array{Any}(undef, length(suffixNames) + 1)
     results[1] = readAnchors(dataName, "Baseline")[queryID] # R
     for solverID in eachindex(suffixNames)
@@ -38,16 +42,20 @@ function generateGephi(B::SparseMatrixCSC, dataName::String, suffixNames::Array{
         degree = GetDegree(B, orgid)
         label = string(degree)
         indicator = map(x->orgid in x, results)
-        color = getColorSchemeKRGB(indicator[1], indicator[3], indicator[4], indicator[5])
+        colorInds = Array{Int}(undef, 3)
+        for i in 1:3
+            colorInds[i] = indicatorIDs[i] > 0 ? indicator[indicatorIDs[i]] : 0
+        end
+        color = getColorSchemeRGB(colorInds[1], colorInds[2], colorInds[3])
         push!(df, vcat([id, orgid, degree, label, color], indicator))
     end
-    CSV.write(string(folderString(FOLDER_CS_LP), join(["cslp", "node", dataName, queryID], "-"), ".csv"), df, header=true)
-    exportGephiEdgelist(B[unionRes, unionRes], string(join(["cslp", "edge", dataName, queryID], "-"), ".csv"), folderString(FOLDER_CS_LP))
+    CSV.write(string(folderString(FOLDER_CS_LP), join(["cslp", "node", dataName, queryID, suffix], "-"), ".csv"), df, header=true)
+    exportGephiEdgelist(B[unionRes, unionRes], string(join(["cslp", "edge", dataName, queryID, suffix], "-"), ".csv"), folderString(FOLDER_CS_LP))
 end
 
 # A color scheme that takes 4 indicators, the first indicator is considered "black base".
 # Each indicator is an array of 0/1s.
-function getColorSchemeKRGB(k, r, g, b)
+function getColorSchemeRGB(r, g, b)
     rc = 0.5 + r * 0.5
     gc = 0.5 + g * 0.5
     bc = 0.5 + b * 0.5

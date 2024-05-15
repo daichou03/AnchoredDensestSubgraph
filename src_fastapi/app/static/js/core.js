b15eb4a280b1be4860b91252c5550a262cffb676
daichou03 <daichou03@gmail.com>
2024-05-15 13:43:10 +1200

    Text area showing R

--- src_fastapi/app/static/js/core.js
+++ src_fastapi/app/static/js/core.js
@@ -1,4 +1,4 @@
-async function sendPostRequest(url, body, headers = {'Content-Type': 'application/x-www-form-urlencoded'}) {
+export async function sendPostRequest(url, body, headers = {'Content-Type': 'application/x-www-form-urlencoded'}) {
     const response = await fetch(url, {
         method: 'POST',
         headers: headers,
@@ -9,19 +9,5 @@ async function sendPostRequest(url, body, headers = {'Content-Type': 'applicatio
         return data;
     } else {
         throw new Error(data.detail || 'Unknown error occurred');
-    }
-}
-
-async function executeQuery() {
-    const databaseName = document.getElementById('database-name').value;
-    const query = document.getElementById('any-query').value;
-    const body = `database_name=${encodeURIComponent(databaseName)}&query=${encodeURIComponent(query)}`;
-    const url = 'http://localhost:8000/execute-query/';
-
-    try {
-        const result = await sendPostRequest(url, body);
-        document.getElementById('result-query').textContent = JSON.stringify(result);
-    } catch (error) {
-        document.getElementById('result-query').textContent = 'Error: ' + error.message;
     }
 }
--- src_fastapi/app/static/js/index.js
+++ src_fastapi/app/static/js/index.js
@@ -1,3 +1,17 @@
+async function executeQuery() {
+    const databaseName = document.getElementById('database-name').value;
+    const query = document.getElementById('any-query').value;
+    const body = `database_name=${encodeURIComponent(databaseName)}&query=${encodeURIComponent(query)}`;
+    const url = 'http://localhost:8000/execute-query/';
+
+    try {
+        const result = await sendPostRequest(url, body);
+        document.getElementById('result-query').textContent = JSON.stringify(result);
+    } catch (error) {
+        document.getElementById('result-query').textContent = 'Error: ' + error.message;
+    }
+}
+
 async function getNodeCount() {
     const databaseName = document.getElementById('database-name').value;
     const body = `database_name=${encodeURIComponent(databaseName)}`;
